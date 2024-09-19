a9377c9e9cdc037e1d3b44d401d6febe7b00bb91
Maik Costa <maikcosta@gmail.com>
2024-09-19 18:36:58 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sso',
+  webDir: 'www'
+};
+
+export default config;
