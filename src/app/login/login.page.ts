d625c501849589d6819eaa312c113e8f85a46d2e
Maik Costa <maikcosta@gmail.com>
2024-09-20 15:52:24 -0300

    Update Readme

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -13,8 +13,26 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  loginWithMicrosoft() {
-    this.msalService.loginRedirect();
+  async loginWithMicrosoft() {
+    try {
+      const result = await this.msalService.instance.handleRedirectPromise();
+  
+      if (result) {
+        console.log('Redicionamento com sucesso!', result);
+      } else {
+        const accounts = this.msalService.instance.getAllAccounts();
+        if (accounts.length === 0) {
+          this.msalService.loginRedirect({
+            scopes: ['user.read'],
+            extraQueryParameters: { useEmbeddedWebView: 'true' }
+          });
+        } else {
+          console.log('Usuário já está logado:', accounts);
+        }
+      }
+    } catch (error) {
+      console.error('Erro ao processar redirecionamento ou login:', error);
+    }
   }
 
 }
